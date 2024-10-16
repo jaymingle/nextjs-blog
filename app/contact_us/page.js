56f376b6ced4f3fe5bea893630f96d7ba4385d7c
Jay Mingle <jkawudi@gmail.com>
2024-10-16 03:43:25 +0000

    Changes

--- app/contact_us/page.js
+++ app/contact_us/page.js
@@ -1,11 +1,11 @@
 import React from 'react';
 
-const Page = () => {
+const ContactUs = () => {
     return (
         <div>
-            
+            <h1>Contact Us</h1>
         </div>
     );
 };
 
-export default Page;
+export default ContactUs;
