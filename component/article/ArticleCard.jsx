703b054748cf2465df7357f1ebf687200c7a28e4
Jay Mingle <jkawudi@gmail.com>
2024-10-24 16:50:55 +0000

    Changes

--- component/article/ArticleCard.jsx
+++ component/article/ArticleCard.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Box, Card, CardContent, Typography, Grid } from '@mui/material';
+import Link from "next/link";
 
 const ArticleCard = ({ title, content, date, category, image }) => {
     return (
@@ -21,22 +22,24 @@ const ArticleCard = ({ title, content, date, category, image }) => {
 
                 {/* Content Column - 70% width */}
                 <Grid item xs={12} sm={8}>
-                    <CardContent sx={{ padding: '1rem' }}> {/* Reduce padding */}
-                        <Typography variant="h6" component="div" gutterBottom> {/* Smaller font for title */}
-                            {title}
-                        </Typography>
-                        <Typography sx={{marginY: 3}} variant="body2" color="text.secondary" paragraph>
-                            {content.length > 100 ? content.slice(0, 100) + '...' : content} {/* Limit content length */}
-                        </Typography>
-                        <Box sx={{ display: 'flex', gap: 2, mt: 1 }}> {/* Reduce margin */}
-                            <Typography variant="body2" color="text.secondary">
-                                {date}
+                    <Link href={`/articles/`}>Post Page
+                        <CardContent sx={{ padding: '1rem' }}> {/* Reduce padding */}
+                            <Typography variant="h6" component="div" gutterBottom> {/* Smaller font for title */}
+                                {title}
                             </Typography>
-                            <Typography variant="body2" color="primary">
-                                {category}
+                            <Typography sx={{marginY: 3}} variant="body2" color="text.secondary" paragraph>
+                                {content.length > 100 ? content.slice(0, 100) + '...' : content} {/* Limit content length */}
                             </Typography>
-                        </Box>
-                    </CardContent>
+                            <Box sx={{ display: 'flex', gap: 2, mt: 1 }}> {/* Reduce margin */}
+                                <Typography variant="body2" color="text.secondary">
+                                    {date}
+                                </Typography>
+                                <Typography variant="body2" color="primary">
+                                    {category}
+                                </Typography>
+                            </Box>
+                        </CardContent>
+                    </Link>
                 </Grid>
             </Grid>
         </Card>
