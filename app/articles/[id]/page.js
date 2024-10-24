773c678ae0dd4046d06cbb6c07cf144569d5aff5
Jay Mingle <jkawudi@gmail.com>
2024-10-24 16:53:17 +0000

    Changes

---
+++ app/articles/[id]/page.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const ArticlePage = () => {
+    return (
+        <div>
+            <h3>The Article Page</h3>
+        </div>
+    );
+};
+
+export default ArticlePage;
