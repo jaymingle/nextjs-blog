e559ab028d67eccb9172fe5dcf1bd106324d1d70
Jay Mingle <jkawudi@gmail.com>
2024-10-25 02:16:15 +0000

    Changes

--- app/create/category/page.js
+++ app/create/category/page.js
@@ -1,10 +1,16 @@
 import React from 'react';
+import {Box, Typography} from "@mui/material";
 
 const CreateCategory = () => {
     return (
-        <div>
-            <h1>Create New Category</h1>
-        </div>
+        <Box sx={{marginY: 5, marginX: 30}}>
+            <Typography variant='h3' sx={{
+                textAlign: 'center',
+                fontSize: '28px',
+                fontWeight: 'bold',
+                marginY: 4,
+            }}>Create New Category</Typography>
+        </Box>
     );
 };
 
