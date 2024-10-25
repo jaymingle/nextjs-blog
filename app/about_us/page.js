2ecb95b09b3756aa7a35e43b28ac127444fa8db2
Jay Mingle <jkawudi@gmail.com>
2024-10-25 01:32:30 +0000

    Changes

--- app/about_us/page.js
+++ app/about_us/page.js
@@ -1,5 +1,8 @@
 import React from 'react';
-import {Box, Button, TextField, Typography} from "@mui/material";
+import {Box, Typography} from "@mui/material";
+import Image from "next/image";
+
+import aboutPageImage from '@/assets/about_us_page.avif'
 
 const AboutUs = () => {
     return (
@@ -10,57 +13,30 @@ const AboutUs = () => {
                 fontWeight: 'bold'
             }}>About This Blog</Typography>
 
-            <Box>
-                <Box
-                    component="form"
-                    sx={{
-                        display: 'flex',
-                        flexDirection: 'column',
-                        gap: '1.5rem',
-                        marginTop: '2rem',
-                    }}
-                    noValidate
-                    autoComplete="off"
-                >
-                    {/* Name Field */}
-                    <TextField
-                        label="Your Name"
-                        variant="outlined"
-                        fullWidth
-                        required
-                    />
+            <Box sx={{marginY: 4}}>
+                <Image sx={{marginY: 4}} src={aboutPageImage} alt='About Us Page' width={1200} height={500}/>
 
-                    {/* Email Field */}
-                    <TextField
-                        label="Your Email"
-                        type="email"
-                        variant="outlined"
-                        fullWidth
-                        required
-                    />
+                <Box sx={{marginX: 10}}>
+                    <Typography variant='body1' sx={{textAlign: 'center', paddingY: 4, lineHeight: 2.5,}}>
+                        Morbi vestibulum volutpat enim. Phasellus nec sem in justo pellentesque
+                        facilisis. <br/> Phasellus tempus. Vestibulum volutpat pretium libero. Nulla sit amet est. <br/>
 
-                    {/* Subject Field */}
-                    <TextField
-                        label="Subject"
-                        variant="outlined"
-                        fullWidth
-                    />
+                        Fusce fermentum odio nec arcu. Cras ultricies mi eu turpis hendrerit fringilla. <br/> Aenean viverra
+                        rhoncus pede. Phasellus consectetuer vestibulum elit. Quisque id odio. <br/>
 
-                    {/* Message Field */}
-                    <TextField
-                        label="Message"
-                        variant="outlined"
-                        multiline
-                        rows={4}
-                        fullWidth
-                        required
-                    />
+                        Cras id dui. Suspendisse potenti. Sed magna purus, fermentum eu, tincidunt eu, varius ut,
+                        felis. <br/> Fusce commodo aliquam arcu. Nullam sagittis.
 
-                    {/* Submit Button */}
-                    <Button variant="contained" color="primary" size="large">
-                        Send Message
-                    </Button>
+                        Pellentesque habitant morbi tristique <br/> senectus et netus et malesuada fames ac turpis
+                        egestas. <br/> Vivamus in erat ut urna cursus vestibulum. <br/> Nam ipsum risus, rutrum vitae,
+                        vestibulum eu, molestie vel, lacus. Aliquam lobortis. <br/> Donec mollis hendrerit risus.
+
+                        Suspendisse faucibus, nunc et pellentesque egestas, lacus ante convallis tellus, <br/> vitae
+                        iaculis lacus elit id tortor. Suspendisse potenti. <br/> Fusce vulputate eleifend sapien. Nulla
+                        consequat massa quis enim. Vestibulum eu odio.
+                    </Typography>
                 </Box>
+
             </Box>
 
         </Box>
--- app/categories/page.js
+++ app/categories/page.js
@@ -1,10 +1,29 @@
 import React from 'react';
+import {Box, Typography} from "@mui/material";
 
 const Categories = () => {
+
+    const categories = [
+        { name: 'Technology', articles: 15 },
+        { name: 'Lifestyle', articles: 10 },
+        { name: 'Business', articles: 8 },
+        { name: 'Health', articles: 12 },
+        { name: 'Travel', articles: 5 },
+    ];
+
+
     return (
-        <div>
-            <h1>Categories</h1>
-        </div>
+        <Box>
+            <Typography variant='h3' sx={{
+                textAlign: 'center',
+                fontSize: '26px',
+                fontWeight: 'bold',
+                marginY: 5
+            }}>Categories</Typography>
+
+
+
+        </Box>
     );
 };
 
