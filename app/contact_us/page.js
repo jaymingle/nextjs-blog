2c702311ce4146b6447fabbb37a036afd9418eb1
Jay Mingle <jkawudi@gmail.com>
2024-10-25 01:00:45 +0000

    Changes

--- app/contact_us/page.js
+++ app/contact_us/page.js
@@ -1,18 +1,16 @@
 import React from 'react';
 import {Box, Typography} from "@mui/material";
-import Link from "next/link";
-import Image from "next/image";
-import blogImage from "@/assets/blog_image2.avif";
-import sendMessage from "@/assets/blog_image_1.avif";
-import createPost from "@/assets/create_post.avif";
-import createCategory from "@/assets/create_category.avif";
+
 
 const ContactUs = () => {
     return (
         <Box sx={{marginY: 5, marginX: 20}}>
-            <Box sx={{display: 'flex', gap: 5}}>
+            <Typography>Send Us A Message</Typography>
+
+            <Box>
 
             </Box>
+
         </Box>
     );
 };
